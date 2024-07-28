ed4a4c10d4569ec23b31788ad5f43a8d1a1d4d8e
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2024-07-28 04:30:01 -0300

    Add language

--- app.py
+++ app.py
@@ -1,31 +1,47 @@
 from flask import Flask, request
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage
 from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 app = Flask(__name__)
 
 load_dotenv()
-
-store = {}
-
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
   if session_id not in store:
     store[session_id] = InMemoryChatMessageHistory()
   return store[session_id]
 
-with_message_history = RunnableWithMessageHistory(llm, get_session_history)
+store = {}
 
-config = {"configurable": {"session_id": "abc2"}}
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+config = {"configurable": {"session_id": "abc"}}
+
+prompt = ChatPromptTemplate.from_messages(
+  [
+    (
+      "system",
+      "Your name is MaggieBot. You are a chatbot made to help Bruno and Letícia, they are fiances. Answear all questions in {language}",
+    ),
+    MessagesPlaceholder(variable_name="messages")
+  ]
+)
+
+chain = prompt| llm
+
+with_message_history = RunnableWithMessageHistory(chain, get_session_history, input_messages_key="messages")
 
 @app.route("/")
 def hello_world():
   response = with_message_history.invoke(
-    [HumanMessage(content=request.args.get("message"))],
+    {
+      "messages": [HumanMessage(content=request.args.get("message"))],
+      "language": "english"
+    },
     config=config
   )
   return response.content
