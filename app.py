7ac5efd43988dcac583e31d78271124bea522918
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2024-07-28 02:44:24 -0300

    chat history

--- app.py
+++ app.py
@@ -2,14 +2,30 @@ from flask import Flask, request
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 app = Flask(__name__)
 
 load_dotenv()
 
+store = {}
+
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+  if session_id not in store:
+    store[session_id] = InMemoryChatMessageHistory()
+  return store[session_id]
+
+with_message_history = RunnableWithMessageHistory(llm, get_session_history)
+
+config = {"configurable": {"session_id": "abc2"}}
 
 @app.route("/")
 def hello_world():
-  response = llm.invoke([HumanMessage(content=request.args.get("message"))])
+  response = with_message_history.invoke(
+    [HumanMessage(content=request.args.get("message"))],
+    config=config
+  )
   return response.content
