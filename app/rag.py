1f5bbe3bcf9297b41b524a2772a085dd303a05de
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2024-08-10 09:33:56 -0300

    Clearing

--- app/rag.py
+++ app/rag.py
@@ -1,7 +1,5 @@
-from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.tracers import ConsoleCallbackHandler
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain import hub
 from app.tool import calculate_percentage
@@ -16,8 +14,6 @@ def handle_chat(messages, language):
 
   agent = create_react_agent(llm=model, tools=tools, prompt=prompt)
   agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
-
-  config = {"callbacks":[ConsoleCallbackHandler()]}
 
   ai_msg = agent_executor.invoke({
     "language": language,
