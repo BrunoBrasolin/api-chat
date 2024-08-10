1998003b6fac90bc084075a7f01111aaccb15f36
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2024-08-10 10:58:23 -0300

    now go

--- app/rag.py
+++ app/rag.py
@@ -1,25 +1,25 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain import hub
 from app.tool import calculate_percentage
 
 def handle_chat(messages, language):
-  model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+  llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
-  search = TavilySearchResults(max_results=2)
-  tools=[search, calculate_percentage]
+  tools=[calculate_percentage]
 
   prompt = hub.pull('brunobrasolin/maggie-bot')
 
-  agent = create_react_agent(llm=model, tools=tools, prompt=prompt)
+  agent = create_react_agent(llm, tools, prompt)
   agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
 
   ai_msg = agent_executor.invoke({
     "language": language,
     "message": messages
   })
+
   print("--------------------------------------------------------")
   print(ai_msg)
   print("--------------------------------------------------------")
+
   return ai_msg
--- app/tool.py
+++ app/tool.py
@@ -2,13 +2,29 @@ from pydantic import BaseModel, Field
 from langchain.tools import tool
 import requests
 
-
 class CalculatePercentage(BaseModel):
-    value: str = Field(description="value in brazilian real to use to calculate the percentage")
+    value: str = Field(description="The total amount for which the percentages need to be calculated. This value should be provided as a string representing a decimal number (e.g., '20.00'). The tool will calculate how much each individual, Bruno and Letícia, should pay based on this total amount.")
 
 @tool("calculate_percentage", args_schema=CalculatePercentage)
-def calculate_percentage(value: int):
-  """Calculate the percentage of a money value between Bruno and Let'icia"""
-  url = f"https://api.gamidas.dev.br/contas/conta?valor={value}"
-  response = requests.get(url)
-  return response.json()
+def calculate_percentage(value: str) -> dict[str, float]:
+  """
+  Tool Description:
+
+  This tool is an API that calculates the proportional division of a total amount between two individuals, Bruno and Letícia. The tool receives a total amount as input and returns a JSON object specifying how much each person should pay. The `Leticia` field indicates the amount Letícia owes, while the `Bruno` field indicates the amount Bruno owes. The values are expressed in decimal format and represent the proportional shares each should pay of the total amount.
+
+  Example Usage:
+
+  - Input: A total amount (e.g., 20.00)
+  - Output: A JSON object like {'Leticia': '4.62', 'Bruno': '15.38'}, indicating that Letícia should pay R$4.62 and Bruno R$15.38 of the total amount.
+  """
+  try:
+    response = requests.get(f"https://api.gamidas.dev.br/contas/conta?valor={value}")
+    response.raise_for_status()
+
+    return response.json()
+    
+  except requests.exceptions.RequestException as e:
+      raise RuntimeError(f"Failed to fetch the percentage calculation. {e}")
+
+  except ValueError as e:
+      raise RuntimeError(f"Failed to parse the response data: {e}.")
