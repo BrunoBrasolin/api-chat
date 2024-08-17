8948021d7df5308942367c45456108162dd40c74
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2024-08-17 04:33:08 -0300

    starting audio

--- app/service.py
+++ app/service.py
@@ -1,5 +1,8 @@
+import os
+import tempfile
 from app.dto import ChatDto
 from app.rag import handle_chat
+import pvleopard
 
 def rag_service(request):
   data = request
@@ -8,3 +11,29 @@ def rag_service(request):
   response = handle_chat(messages = dto.message, language = dto.language)
 
   return {"message": response['output'], "language": dto.language}
+
+def transcript_audio(audio_storage):
+  current_directory = os.path.dirname(os.path.abspath(__file__))
+  model_path = os.path.join(current_directory, "dalme-leopard.pv")
+
+  with tempfile.NamedTemporaryFile(delete=False, suffix=".pv") as temp_file:
+    file_path = temp_file.name
+    audio_storage.save(file_path)
+
+  leopard = pvleopard.create(
+    access_key=os.getenv('LEOPARD_API_KEY'),
+    model_path=model_path)
+
+  transcript, words = leopard.process_file(file_path)
+  print(transcript)
+  for word in words:
+    print(
+      "{word=\"%s\" start_sec=%.2f end_sec=%.2f confidence=%.2f}"
+      % (word.word, word.start_sec, word.end_sec, word.confidence))
+
+  leopard.delete()
+  os.remove(file_path)
+
+  response = handle_chat(messages=transcript, language="portuguese")
+
+  return {"message": response['output'], "language": "portuguese"}
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS, cross_origin
 
-from app.service import rag_service
+from app.service import rag_service, transcript_audio
 
 app = Flask(__name__)
+cors = CORS(app)
 
 load_dotenv("/app/config/.env")
 
-@app.route("/", methods=["POST"])
-def chat():
+@app.route("/message", methods=["POST"])
+@cross_origin()
+def message():
   result = rag_service(request.get_json())
+  return result
+
+@app.route("/audio", methods=["POST"])
+@cross_origin()
+def audio():
+  audio_file = request.files['audio']
+  result = transcript_audio(audio_file)
   return result
 
 @app.route("/healthcheck", methods=["GET"])
