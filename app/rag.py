8bb7a3934b9f5c840a263c8bcca5fdc3377bf44f
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2024-08-17 01:50:41 -0300

    AGORA SIM!!!

--- app/rag.py
+++ app/rag.py
@@ -1,18 +1,29 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.tools import TavilySearchResults
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain import hub
 from app.tool import calculate_percentage
 
 def handle_chat(messages, language):
-  llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+  search_tool = TavilySearchResults(max_results=2)
 
-  tools=[calculate_percentage]
+  tools=[calculate_percentage, search_tool]
 
-  prompt = hub.pull('brunobrasolin/maggie-bot')
+  llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash").bind_tools(tools)
+
+  prompt = hub.pull('brunobrasolin/delma')
 
   agent = create_react_agent(llm, tools, prompt)
-  agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
 
+  agent_executor = AgentExecutor(
+                    agent=agent,
+                    tools=tools,
+                    verbose=True,
+                    handle_parsing_errors=True,
+                    max_execution_time=30,
+                    max_iterations=3
+                  )
+  
   ai_msg = agent_executor.invoke({
     "language": language,
     "message": messages
