993c677e16e224d0eedb7cb171236d684fc34be4
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2024-08-18 22:51:31 -0300

    Test CORS

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 from flask import Flask, request
 from dotenv import load_dotenv
-from flask_cors import CORS, cross_origin
 
 from app.service import rag_service, transcript_audio
 
 app = Flask(__name__)
-cors = CORS(app)
 
 load_dotenv("/app/config/.env")
 
 @app.route("/message", methods=["POST"])
-@cross_origin()
 def message():
   result = rag_service(request.get_json())
   return result
 
 @app.route("/transcript_audio", methods=["POST"])
-@cross_origin()
 def audio():
   audio = request.files['audio']
   result = transcript_audio(audio)
